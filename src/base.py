1c0d658a7b166b4c761e92d2727afb205833048c
RasmusUni <202004598@post.au.dk>
2022-09-11 21:06:54 +0200

    base

--- src/base.py
+++ src/base.py
@@ -18,6 +18,7 @@ def change_to_base(n: int, b: int) -> str:
     Return `n` in base `b`.
 
     The base `b` must be in the range 2 to 16.
+    
 
     >>> change_to_base(1, 2)
     '1'
@@ -28,5 +29,26 @@ def change_to_base(n: int, b: int) -> str:
     >>> change_to_base(31, 16)
     '1F'
     """
+    if n >= 0:
+        z=[]
+
+        while n // b != 0:
+
+            z.append(n % b)
+            n=n//b
+
+        z.append(n % b)
+
+        z=z[::-1]
+
+        for i in range(len(z)):
+            z[i]=digits[z[i]]
+        
+    
+        z="".join(z)
+    else:
+        z="f**k off"
+    
+
     assert 2 <= b <= 16
-    return ''  # FIXME: return n in the right base
+    return str(z)  # FIXME: return n in the right base
